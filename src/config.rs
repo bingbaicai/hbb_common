abf595d13ce073e31829cfa9402d4a58f32f19d2
bingbaicai <275456082@qq.com>
2025-04-29 15:57:31 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.31.41"];
+pub const RS_PUB_KEY: &str = "ftk4CZu4VVEpF4+v8vd5bQkyf+uVsWm6Bd6D6Y7+mrI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
